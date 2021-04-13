518b49665949be031f01afe00f56572bb050ca09
markpsmi <markpsmi@cisco.com>
2021-04-13 14:12:49 -0700

    Update vm.tf

--- vmware/vm.tf
+++ vmware/vm.tf
@@ -38,13 +38,6 @@ resource "vsphere_virtual_machine" "vm" {
     adapter_type = "data.vsphere_virtual_machine.template.network_interface_types[0]"
   }
 
-  disk {
-    label            = "disk0"
-    size             = "data.vsphere_virtual_machine.template.disks.0.size"
-    eagerly_scrub    = "data.vsphere_virtual_machine.template.disks.0.eagerly_scrub"
-    thin_provisioned = "data.vsphere_virtual_machine.template.disks.0.thin_provisioned"
-  }
-
   clone {
     template_uuid = "data.vsphere_virtual_machine.template.id"
 
