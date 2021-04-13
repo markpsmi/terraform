5862a9645d81bf857d8db575920c08648a0e9999
markpsmi <markpsmi@cisco.com>
2021-04-13 14:09:23 -0700

    Update vm.tf

--- vmware/vm.tf
+++ vmware/vm.tf
@@ -4,49 +4,49 @@ data "vsphere_datacenter" "dc" {
 
 data "vsphere_datastore" "datastore" {
   name          = "UCSDMarksmiBIGDS292"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = "data.vsphere_datacenter.dc.id"
 }
 
 data "vsphere_compute_cluster" "cluster" {
   name          = "MarkpsmiCL"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = "data.vsphere_datacenter.dc.id"
 }
 
 data "vsphere_network" "network" {
   name          = "VM Network"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = "data.vsphere_datacenter.dc.id"
 }
 
 data "vsphere_virtual_machine" "template" {
   name          = "CentOS7-Temp"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = "data.vsphere_datacenter.dc.id"
 }
 
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
-  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  resource_pool_id = "data.vsphere_compute_cluster.cluster.resource_pool_id"
+  datastore_id     = "data.vsphere_datastore.datastore.id"
 
   num_cpus = 2
   memory   = 1024
-  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+  guest_id = "data.vsphere_virtual_machine.template.guest_id"
 
-  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+  scsi_type = "data.vsphere_virtual_machine.template.scsi_type"
 
   network_interface {
-    network_id   = "${data.vsphere_network.network.id}"
-    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+    network_id   = "data.vsphere_network.network.id"
+    adapter_type = "data.vsphere_virtual_machine.template.network_interface_types[0]"
   }
 
   disk {
     label            = "disk0"
-    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
-    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
-    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+    size             = "data.vsphere_virtual_machine.template.disks.0.size"
+    eagerly_scrub    = "data.vsphere_virtual_machine.template.disks.0.eagerly_scrub"
+    thin_provisioned = "data.vsphere_virtual_machine.template.disks.0.thin_provisioned"
   }
 
   clone {
-    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    template_uuid = "data.vsphere_virtual_machine.template.id"
 
     customize {
       linux_options {
@@ -59,7 +59,7 @@ resource "vsphere_virtual_machine" "vm" {
         ipv4_netmask = 20
       }
 
-      ipv4_gateway = "172.16.58.254"
+      ipv4_gateway = "172.16.50.254"
     }
   }
 }
