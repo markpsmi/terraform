e62d8c0b3835344d6d7c0079e7fd45a866cf8314
markpsmi <markpsmi@cisco.com>
2021-04-14 10:15:37 -0700

    Update vm.tf

--- vmware/vm.tf
+++ vmware/vm.tf
@@ -4,58 +4,43 @@ data "vsphere_datacenter" "dc" {
 
 data "vsphere_datastore" "datastore" {
   name          = "UCSDMarksmiBIGDS292"
-  datacenter_id = "data.vsphere_datacenter.dc.id"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_compute_cluster" "cluster" {
   name          = "MarkpsmiCL"
-  datacenter_id = "data.vsphere_datacenter.dc.id"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
   name          = "VM Network"
-  datacenter_id = "data.vsphere_datacenter.dc.id"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_virtual_machine" "template" {
   name          = "CentOS7-Temp"
-  datacenter_id = "data.vsphere_datacenter.dc.id"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
-  resource_pool_id = "data.vsphere_compute_cluster.cluster.resource_pool_id"
-  datastore_id     = "data.vsphere_datastore.datastore.id"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
 
   num_cpus = 2
   memory   = 1024
-  guest_id = "data.vsphere_virtual_machine.template.guest_id"
+  guest_id = data.vsphere_virtual_machine.template.guest_id
 
-  scsi_type = "lsilogic"
-
-  network_interface {
-    network_id   = "data.vsphere_network.network.id"
+    network_interface {
+    network_id   = data.vsphere_network.network.id
     adapter_type = "vmxnet3"
   }
 disk {
     label            = "disk0"
-    size             = "40"
+    size             = "100"
   }
   clone {
-    template_uuid = "data.vsphere_virtual_machine.template.id"
+    template_uuid = data.vsphere_virtual_machine.template.id
 
-    customize {
-      linux_options {
-        host_name = "terraform-test"
-        domain    = "test.internal"
-      }
-
-      network_interface {
-        ipv4_address = "172.16.58.240"
-        ipv4_netmask = 20
-      }
-
-      ipv4_gateway = "172.16.50.254"
     }
-  }
 }
