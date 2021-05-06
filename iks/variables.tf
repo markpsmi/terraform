c20b4f81cb35092b64fb66356fb2393e0ad58a32
markpsmi <markpsmi@cisco.com>
2021-05-06 13:14:28 -0700

    Update variables.tf

--- iks/variables.tf
+++ iks/variables.tf
@@ -1,8 +1,8 @@
 variable "api_key_id" {
-  default = "5a53d2043768393836b871c1/5b7c7d7e663471746d0ee3a4/5f62651e7564612d32771e7d"
+  type = string
 }
 variable "api_private_key" {
-  default = "/mnt/c/Users/markpsmi/Desktop/Intersight/intersight-rest-api/key/SecretKey.txt"
+  type = string
 }
 variable "api_endpoint" {
   default = "https://www.intersight.com"
