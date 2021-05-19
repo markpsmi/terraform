e87ec6c713747adbdd643cb99a2e6ea33779ecb1
markpsmi <markpsmi@cisco.com>
2021-05-19 11:10:18 -0700

    Create variables.tf

---
+++ vmware_richfield/variables.tf
@@ -0,0 +1,19 @@
+variable "vsphere_user" {
+  type = string
+  description = "vCenter User ID"
+}
+variable "vsphere_password" {
+  type = string
+  description = "vCenter User Password"
+}
+variable "vsphere_server" {
+  type = string
+  description = "vCenter Host/IP"
+}
+variable "vm_name" {
+  type = string
+  description = "VM Name"
+}
+
+
+
