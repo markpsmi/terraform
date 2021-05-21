88cbb88c3ef33ffd897705d5cb301cdf63304b20
markpsmi <markpsmi@cisco.com>
2021-05-21 12:21:25 -0700

    Update vm.tf

--- vmware_richfield/vm.tf
+++ vmware_richfield/vm.tf
@@ -18,7 +18,7 @@ data "vsphere_network" "network" {
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "Centos8-Template"
+  name          = "ubuntu18-temp"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
