037f31365693a947475423570a5048c4f4fba11b
markpsmi <markpsmi@cisco.com>
2021-06-10 16:00:10 -0700

    Create variables.tf

---
+++ Training/variables.tf
@@ -0,0 +1,15 @@
+variable "apikey" {
+  default = ""
+}
+variable "secretkeyfile" {
+  default = ""
+}
+variable "endpoint" {
+  default = "https://www.intersight.com"
+}
+variable "organization_name" {
+  default = "default"
+}
+variable "organization" {
+  default= "default"
+}  
