72c631d89e62c1d488fa88d0a89370b9d6742b73
markpsmi <markpsmi@cisco.com>
2021-06-28 13:42:33 -0700

    Create main.tf

---
+++ training/main.tf
@@ -0,0 +1,22 @@
+# Intersight Provider Information 
+terraform {
+  required_providers {
+    intersight = {
+      source  = "ciscodevnet/intersight"
+      version = ">= 0.1.0"
+    }
+  }
+}
+
+provider "intersight" {
+  apikey        = var.api_key_id
+  secretkey     = var.api_private_key
+  endpoint      = var.api_endpoint
+}
+data "intersight_organization_organization" "organization_moid" {
+  name = "default"
+}
+
+output "organization_moid" {
+  value = data.intersight_organization_organization.organization_moid.moid
+}
