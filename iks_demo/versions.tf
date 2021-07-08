4d107136998c4c2046d293bea9026ee5f5ddd4a1
markpsmi <markpsmi@cisco.com>
2021-07-08 09:22:03 -0700

    Create versions.tf

---
+++ iks_demo/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">=0.14.5"
+
+  required_providers {
+    intersight = {
+      source  = "CiscoDevNet/intersight"
+      version = ">=1.0.11"
+    }
+  }
+}
+
+    © 2021 GitHub, Inc.
+    Terms
+    Privacy
+    Security
