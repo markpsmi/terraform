a9666ede01f331c135e974035e46e09bb51a3516
markpsmi <markpsmi@cisco.com>
2021-07-13 11:59:28 -0700

    Create variables.tf

---
+++ aws/demoapp/variables.tf
@@ -0,0 +1,19 @@
+variable "rootPassword" {
+  type=string
+}
+
+variable "password" {
+    type=string
+}
+
+variable "username" {
+    type=string
+}
+
+variable "database" {
+    type=string
+}
+
+variable "replicaSetKey" {
+    type=string
+}
