b67c6316b99cdc8db86547706e3b721eaf5d417f
markpsmi <markpsmi@cisco.com>
2021-07-14 15:41:36 -0700

    Create redis-leader.tf

---
+++ aws/guestbook/redis-leader.tf
@@ -0,0 +1,67 @@
+resource "kubernetes_deployment" "leader" {
+  metadata {
+    name = "redis-leader"
+    labels = {
+      app = "redis"
+      role = "leader"
+      tier = "backend"
+    }
+  }  
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app  = "redis"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "redis"
+          role = "leader"
+          tier = "backend"                  
+        }
+      }
+
+      spec {
+        container {
+          image = "docker.io/redis:6.0.5"
+          name  = "follower"
+
+          resources {
+            requests = {
+              cpu    = "100m"
+              memory = "100Mi"
+            } 
+          }             
+          port {
+          container_port = "6379"
+          }         
+        }                 
+      }
+    }
+  }
+}
+resource "kubernetes_service" "leader" {
+  metadata {
+    name = "redis-leader"
+    labels = {
+      app = "redis"
+      role = "leader"
+      tier = "backend"
+    }   
+  }
+  spec {
+    selector = {
+      app = "redis"
+      role = "leader"
+      tier = "backend"
+    }
+    port {
+      port = 6379
+      target_port = 6379
+    }
+  }
+}
