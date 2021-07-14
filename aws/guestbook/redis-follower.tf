f4776283a930f8e24f522afbdc9fc68a06aaf33a
markpsmi <markpsmi@cisco.com>
2021-07-14 15:40:07 -0700

    Create redis-follower.tf

---
+++ aws/guestbook/redis-follower.tf
@@ -0,0 +1,67 @@
+resource "kubernetes_deployment" "follower" {
+  metadata {
+    name = "redis-follower"
+    labels = {
+      app = "redis"
+      role = "follower"
+      tier = "backend"
+    }
+  }  
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app  = "redis"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "redis"
+          role = "follower"
+          tier = "backend"                  
+        }
+      }
+
+      spec {
+        container {
+          image = "gcr.io/google_samples/gb-redis-follower:v2"
+          name  = "follower"
+
+          resources {
+            requests = {
+              cpu    = "100m"
+              memory = "100Mi"
+            } 
+          }             
+          port {
+          container_port = "6379"
+          }         
+        }                 
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "follower" {
+  metadata {
+    name = "redis-follower"
+    labels = {
+      app = "redis"
+      role = "follower"
+      tier = "backend"
+    }   
+  }
+  spec {
+    selector = {
+      app = "redis"
+      role = "follower"
+      tier = "backend"
+    }
+    port {
+      port        = 6379
+    }
+  }
+}
