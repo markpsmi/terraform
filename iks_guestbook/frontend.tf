76793d4b800ddeae5d7cfe96568e0de66017174a
markpsmi <markpsmi@cisco.com>
2021-07-21 11:42:33 -0700

    Create frontend.tf

---
+++ iks_guestbook/frontend.tf
@@ -0,0 +1,69 @@
+resource "kubernetes_deployment" "frontend" {
+  metadata {
+    name = "frontend"
+    labels = {
+      app = "guestbook"
+      tier = "frontend"
+    }
+  }  
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        app  = "guestbook"
+        tier = "frontend"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "guestbook"
+          tier = "frontend"                  
+        }
+      }
+
+      spec {
+        container {
+          image = "gcr.io/google_samples/gb-frontend:v5"
+          name  = "php-redis"
+
+        env {
+          name = "GET_HOST_FROM"
+          value = "dns"
+        }
+          resources {
+            requests = {
+              cpu    = "100m"
+              memory = "100Mi"
+            } 
+          }             
+          port {
+          container_port = "80"
+          }         
+        }                 
+      }
+    }
+  }
+}
+resource "kubernetes_service" "frontend" {
+  metadata {
+    name = "frontend"
+    labels = {
+      app = "guestbook"
+      tier = "frontend"
+    }   
+  }
+  spec {
+    selector = {
+      app = "guestbook"
+      tier = "frontend"
+    }
+    type = "LoadBalancer"
+
+    port {
+      port        = 80
+    }
+  }
+}
