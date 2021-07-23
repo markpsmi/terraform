ddae17f263cf708aba60bae44cfe9ae239c1033c
markpsmi <markpsmi@cisco.com>
2021-07-23 15:50:33 -0700

    Update variable.tf

--- iks_guestbook2/variable.tf
+++ iks_guestbook2/variable.tf
@@ -1,5 +1,5 @@
 variable "apikey" {
-  default = "5a53d2043768393836b871c1/5b7c7d7e663471746d0ee3a4/5f62651e7564612d32771e7d"
+  type = "5a53d2043768393836b871c1/5b7c7d7e663471746d0ee3a4/5f62651e7564612d32771e7d"
 }
 variable "secretkey" {
   type  = string
