5caad0b4cfab1780ae7c767bf5e7396fd86120c7
markpsmi <markpsmi@cisco.com>
2021-07-23 11:58:50 -0700

    Update variable.tf

--- iks_guestbook/variable.tf
+++ iks_guestbook/variable.tf
@@ -1,3 +1,11 @@
 variable "cluster_yaml" {
   type    = string
 }
+variable "api_key_id" {
+  type  = string
+}
+variable "api_private_key" {
+  type = string
+}
+variable "api_endpoint" {
+  default = "https://www.intersight.com"
