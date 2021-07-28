24902b9b7246f4c33a39ebbff959962de318ea19
markpsmi <markpsmi@cisco.com>
2021-07-28 12:22:59 -0700

    Create frontend.tf

---
+++ aws/guestbook/frontend.tf
@@ -0,0 +1,89 @@
+resource "kubernetes_service" "frontend" {
+
+  metadata {
+    name = "frontend"
+    labels = {
+      app = "guestbook"
+      tier = "frontend"
+    }   
+  }
+  spec {
+    selector = {
+      app = "guestbook"
+      tier = "frontend"
+    }
+    type = "LoadBalancer"
+
+    port {
+      port        = 80
+    }
+  }
+}
+resource "kubernetes_deployment" "follower" {
+  metadata {
+    name = "redis-follower"
+    labels = {
+      app = "redis"
+      role = "follower"
+      tier = "backend"
+    }
+  }  
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app  = "redis"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "redis"
+          role = "follower"
+          tier = "backend"                  
+        }
+      }
+
+      spec {
+        container {
+          image = "gcr.io/google_samples/gb-redis-follower:v2"
+          name  = "follower"
+
+          resources {
+            requests = {
+              cpu    = "100m"
+              memory = "100Mi"
+            } 
+          }             
+          port {
+          container_port = "6379"
+          }         
+        }                 
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "follower" {
+  metadata {
+    name = "redis-follower"
+    labels = {
+      app = "redis"
+      role = "follower"
+      tier = "backend"
+    }   
+  }
+  spec {
+    selector = {
+      app = "redis"
+      role = "follower"
+      tier = "backend"
+    }
+    #type = "LoadBalancer"
+    port {
+      port        = 6379
+    }
+  }
+}
