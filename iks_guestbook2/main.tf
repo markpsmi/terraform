b23081c60c5a106a0d6feb0e090ac9972c93bc23
markpsmi <markpsmi@cisco.com>
2021-07-28 11:12:51 -0700

    Update main.tf

--- iks_guestbook2/main.tf
+++ iks_guestbook2/main.tf
@@ -13,7 +13,7 @@ provider "kubernetes" {
   host                   = yamldecode(base64decode(data.intersight_kubernetes_cluster.terraform-iks.results.0.kube_config)).clusters.0.cluster.server
   cluster_ca_certificate = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.terraform-iks.results.0.kube_config)).clusters.0.cluster.certificate-authority-data)
   client_certificate     = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.terraform-iks.results.0.kube_config)).users[0].user.client-certificate-data)
-  client_key             = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.terrform-iks.results.0.kube_config)).users[0].user.client-key-data)
+  client_key             = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.terraform-iks.results.0.kube_config)).users[0].user.client-key-data)
 }
 resource "kubernetes_deployment" "frontend" {
 
