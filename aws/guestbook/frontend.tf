37b59f45e94356943d0f0187ede7d234a63a4152
markpsmi <markpsmi@cisco.com>
2021-07-28 12:26:28 -0700

    Update frontend.tf

--- aws/guestbook/frontend.tf
+++ aws/guestbook/frontend.tf
@@ -1,3 +1,53 @@
+resource "kubernetes_deployment" "frontend" {
+
+
+  metadata {
+    name = "frontend"
+    labels = {
+      app = "guestbook"
+      tier = "frontend"
+    }
+  }  
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app  = "guestbook"
+        tier = "frontend"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "guestbook"
+          tier = "frontend"                  
+        }
+      }
+
+      spec {
+        container {
+          image = "gcr.io/google_samples/gb-frontend:v5"
+          name  = "php-redis"
+       
+         env {
+          name = "GET_HOST_FROM"
+          value = "dns"
+        }
+        resources {
+            requests = {
+              cpu    = "100m"
+              memory = "100Mi"
+            } 
+          }             
+          port {
+          container_port = "80"
+          }         
+        }                 
+      }
+    }
+  }
+}
 resource "kubernetes_service" "frontend" {
 
   metadata {
@@ -16,74 +66,6 @@ resource "kubernetes_service" "frontend" {
 
     port {
       port        = 80
-    }
-  }
-}
-resource "kubernetes_deployment" "follower" {
-  metadata {
-    name = "redis-follower"
-    labels = {
-      app = "redis"
-      role = "follower"
-      tier = "backend"
-    }
-  }  
-  spec {
-    replicas = 2
-
-    selector {
-      match_labels = {
-        app  = "redis"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "redis"
-          role = "follower"
-          tier = "backend"                  
-        }
-      }
-
-      spec {
-        container {
-          image = "gcr.io/google_samples/gb-redis-follower:v2"
-          name  = "follower"
-
-          resources {
-            requests = {
-              cpu    = "100m"
-              memory = "100Mi"
-            } 
-          }             
-          port {
-          container_port = "6379"
-          }         
-        }                 
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "follower" {
-  metadata {
-    name = "redis-follower"
-    labels = {
-      app = "redis"
-      role = "follower"
-      tier = "backend"
-    }   
-  }
-  spec {
-    selector = {
-      app = "redis"
-      role = "follower"
-      tier = "backend"
-    }
-    #type = "LoadBalancer"
-    port {
-      port        = 6379
     }
   }
 }
