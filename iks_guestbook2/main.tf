8951866cb404ae8eb3756b4be01d1ce0e52f1fa0
markpsmi <markpsmi@cisco.com>
2021-07-28 10:02:02 -0700

    Update main.tf

--- iks_guestbook2/main.tf
+++ iks_guestbook2/main.tf
@@ -4,16 +4,16 @@ provider "intersight" {
   endpoint      = var.endpoint
 }
 
-data "intersight_kubernetes_cluster" "Expedia-iks" {
-  name = "Expedia-iks"
+data "intersight_kubernetes_cluster" "Terraform-iks" {
+  name = "Terraform-iks"
 }
 
 provider "kubernetes" {
 
-  host                   = yamldecode(base64decode(data.intersight_kubernetes_cluster.Expedia-iks.results.0.kube_config)).clusters.0.cluster.server
-  cluster_ca_certificate = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.Expedia-iks.results.0.kube_config)).clusters.0.cluster.certificate-authority-data)
-  client_certificate     = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.Expedia-iks.results.0.kube_config)).users[0].user.client-certificate-data)
-  client_key             = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.Expedia-iks.results.0.kube_config)).users[0].user.client-key-data)
+  host                   = yamldecode(base64decode(data.intersight_kubernetes_cluster.Terraform-iks.results.0.kube_config)).clusters.0.cluster.server
+  cluster_ca_certificate = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.Terraform-iks.results.0.kube_config)).clusters.0.cluster.certificate-authority-data)
+  client_certificate     = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.Terraform-iks.results.0.kube_config)).users[0].user.client-certificate-data)
+  client_key             = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.Terraform-iks.results.0.kube_config)).users[0].user.client-key-data)
 }
 resource "kubernetes_deployment" "frontend" {
 
