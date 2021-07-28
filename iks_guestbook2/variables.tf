55a51581fd264c00fc16e3f80ac41712a974fe07
markpsmi <markpsmi@cisco.com>
2021-07-28 12:13:02 -0700

    Update variables.tf

--- iks_guestbook2/variables.tf
+++ iks_guestbook2/variables.tf
@@ -7,9 +7,3 @@ variable "secretkey" {
 variable "endpoint" {
   default = "https://www.intersight.com"
 }  
-variable "redis_follower_host" {
-  type =  string
-}  
-variable "redis_leader_host" {
-  type =  string
-}  
