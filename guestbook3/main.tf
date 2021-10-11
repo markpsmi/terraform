e874966235eaac7d356ea4059a82206373adc689
markpsmi <markpsmi@cisco.com>
2021-10-11 15:09:05 -0700

    Update main.tf

--- guestbook3/main.tf
+++ guestbook3/main.tf
@@ -4,16 +4,16 @@ provider "intersight" {
   endpoint      = var.endpoint
 }
 
-data "intersight_kubernetes_cluster" "asu-iks-demo" {
-  name = "asu-iks-demo"
+data "intersight_kubernetes_cluster" "marks-iks-cluster" {
+  name = "marks-iks-cluster"
 }
 
 provider "kubernetes" {
 
-  host                   = yamldecode(base64decode(data.intersight_kubernetes_cluster.asu-iks-demo.results.0.kube_config)).clusters.0.cluster.server
-  cluster_ca_certificate = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.asu-iks-demo.results.0.kube_config)).clusters.0.cluster.certificate-authority-data)
-  client_certificate     = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.asu-iks-demo.results.0.kube_config)).users[0].user.client-certificate-data)
-  client_key             = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.asu-iks-demo.results.0.kube_config)).users[0].user.client-key-data)
+  host                   = yamldecode(base64decode(data.intersight_kubernetes_cluster.marks-iks-cluster.results.0.kube_config)).clusters.0.cluster.server
+  cluster_ca_certificate = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.marks-iks-cluster.results.0.kube_config)).clusters.0.cluster.certificate-authority-data)
+  client_certificate     = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.marks-iks-cluster.results.0.kube_config)).users[0].user.client-certificate-data)
+  client_key             = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.marks-iks-cluster.results.0.kube_config)).users[0].user.client-key-data)
 }
 resource "kubernetes_deployment" "frontend" {
 
