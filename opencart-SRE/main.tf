4efb1380b19bfb85be6b113fbd299ed2551bcf3c
markpsmi <markpsmi@cisco.com>
2021-11-18 13:45:27 -0800

    Update main.tf

--- opencart-SRE/main.tf
+++ opencart-SRE/main.tf
@@ -49,11 +49,6 @@ data "vsphere_datastore" "datastore" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_resource_pool" "pool" {
-  name          = var.resource_pool
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
 data "vsphere_network" "network" {
   name          = var.network_name
   datacenter_id = data.vsphere_datacenter.dc.id
