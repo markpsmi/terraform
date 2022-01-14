210004035ac50709d769ebd11e2661f7c9c0da80
markpsmi <markpsmi@cisco.com>
2022-01-14 14:26:19 -0800

    Create variables.tf

---
+++ iks2.1.0/variables.tf
@@ -0,0 +1,30 @@
+variable "apikey" {
+  type        = string
+  description = "API Key"
+}
+variable "secretkey" {
+  type        = string
+  description = "Secret Key or file location"
+}
+variable "endpoint" {
+  type        = string
+  description = "API Endpoint URL"
+  default     = "https://www.intersight.com"
+}
+variable "organization" {
+  type        = string
+  description = "Organization Name"
+  default     = "default"
+}
+variable "ssh_user" {
+  type        = string
+  description = "SSH Username for node login."
+}
+variable "ssh_key" {
+  type        = string
+  description = "SSH Public Key to be used to node login."
+}
+variable "tags" {
+  type    = list(map(string))
+  default = []
+}
