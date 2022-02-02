35307f8c29927c4867b21b8e4c8be4ed83885c85
markpsmi <markpsmi@cisco.com>
2022-02-02 11:40:35 -0800

    Update maint.tf

--- helm-opencart/maint.tf
+++ helm-opencart/maint.tf
@@ -1,6 +1,19 @@
+provider "intersight" {
+  apikey        = var.apikey
+  secretkey     = var.secretkey
+  endpoint      = var.endpoint
+}
+
+data "intersight_kubernetes_cluster" "Marks_iks_cluster2" {
+  name = "Marks_iks_cluster2"
+}
+
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+   host                   = yamldecode(base64decode(data.intersight_kubernetes_cluster.Marks_iks_cluster2.results.0.kube_config)).clusters.0.cluster.server
+  cluster_ca_certificate = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.Marks_iks_cluster2.results.0.kube_config)).clusters.0.cluster.certificate-authority-data)
+  client_certificate     = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.Marks_iks_cluster2.results.0.kube_config)).users[0].user.client-certificate-data)
+  client_key             = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.Marks_iks_cluster2.results.0.kube_config)).users[0].user.client-key-data)
   }
 }
 
