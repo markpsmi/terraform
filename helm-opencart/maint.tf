1d4820e242471372cacb72cf49e21e9571c3bffa
markpsmi <markpsmi@cisco.com>
2022-02-02 11:37:27 -0800

    Create maint.tf

---
+++ helm-opencart/maint.tf
@@ -0,0 +1,17 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
