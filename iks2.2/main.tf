d17fb3089e844459af710b9017ac12f2b09d9381
markpsmi <markpsmi@cisco.com>
2022-03-21 11:50:36 -0700

    Update main.tf

--- iks2.2/main.tf
+++ iks2.2/main.tf
@@ -11,7 +11,7 @@ module "terraform-intersight-iks" {
 
 # Kubernetes Cluster Profile  Adjust the values as needed.
   cluster = {
-    name                = "SEVT_cluster"
+    name                = "TSA_cluster"
     action              = "Deploy"
     wait_for_completion = false
     worker_nodes        = 2
