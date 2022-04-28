8f02e419a470d19d97cc9e2865f0b3a242cfb060
markpsmi <markpsmi@cisco.com>
2022-04-28 12:27:39 -0700

    Update main.tf

--- iks2.2/main.tf
+++ iks2.2/main.tf
@@ -37,7 +37,7 @@ module "iks_cluster" {
 
 # Kubernetes Cluster Profile  Adjust the values as needed.
   cluster = {
-    name                = "CloudOPS"
+    name                = "CloudSecure"
     action              = "Assigned"
     wait_for_completion = false
     worker_nodes        = 2
